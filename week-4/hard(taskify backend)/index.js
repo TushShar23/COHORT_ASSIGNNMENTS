44c3c040950519b8d478b0ea1633fa0513d4e4eb
Tushar Sharma <onlyforcode23@gmail.com>
2025-10-21 19:09:30 +0100

    write index.js file

---
+++ week-4/hard(taskify backend)/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const userRoutes = require("./routes/user") 
+const todoRoutes = require("./routes/todo")
+const mongoose = require("mongoose")
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(express.json());
+
+// app.get("/healthy", (req, res)=> res.send("I am Healthy"));
+
+//  start writing your routes here
+app.use("/api/v1/user",userRoutes)
+app.use("/api/v1/todo",todoRoutes)
+
+
+async function main(){
+    await mongoose.connect(`${process.env.MONGO_URL}/${process.env._DB}`) // Here i have used environment variables.
+    app.listen(port,()=>{
+        console.log("Server Started")
+    })
+}
+
+main()
+
